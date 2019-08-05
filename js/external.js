865b63fc0c611e0d78aedb14b2c51cdfba2dad60
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-05 16:10:38 -0500

    completed external_js and inline_js exercise

---
+++ js/external.js
@@ -0,0 +1,90 @@
+"use strict";
+
+// console.log("Hello from external Javascript");
+//
+// alert("Welcome to my Website!");
+//
+// var userFavoriteColor = prompt("What is your favorite color?");
+//
+// alert("Wow! " + userFavoriteColor + " is my favorite color too!");
+
+// Complete exercise 3 from the previous lesson, but write your code in the external.js file instead of in the console.
+//
+//     When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
+//     Use an alert to show the results of each problem.
+
+// Rented Movie Problem
+
+// var littleMermaidDaysRented = prompt("How many days are you renting The Little Mermaid for?");
+//
+// var brotherBearDaysRented = prompt("How many days are you renting Brother Bear for?");
+//
+// var herculesDaysRented = prompt("How many days are you renting Hercules for?");
+//
+// var pricePerDayDollars = prompt("How many dollars per day is each rental");
+//
+// var rentalTotal = (Number(littleMermaidDaysRented) + Number(brotherBearDaysRented) + Number(herculesDaysRented)) * pricePerDayDollars;
+//
+// alert("Your total is: " + rentalTotal + " Dollars");
+
+
+// Contractor and 3 Companies
+
+// var googleDollarsPerHour = prompt("How much does Google pay per hour?");
+// var amazonDollarsPerHour = prompt("How much does Amazon pay per hour?");
+// var facebookDollarsPerHour = prompt("How much does Facebook pay per hour?");
+//
+// var googleHoursWorked = prompt("How many hours did you work with Google?");
+// var amazonHoursWorked = prompt("How many hours did you work with Amazon?");
+// var facebookHoursWorked = prompt("How many hours did you work with Facebook?");
+//
+// var totalPayDollars = (googleDollarsPerHour * googleHoursWorked) + (amazonDollarsPerHour * amazonHoursWorked) + (facebookDollarsPerHour * facebookHoursWorked);
+//
+// alert("Congratulations you earned " + totalPayDollars + " dollars");
+
+// Student Enrolled in class
+
+//Can this student enroll in the class?
+
+// alert("Lets see if you are eligible to enroll");
+//
+// var isClassFull = confirm("Is the class full");
+//
+// var isScheduleConflict = confirm("Does your schedule conflict with the class");
+//
+// var studentCanEnroll = isClassFull && isScheduleConflict;
+//
+// if (studentCanEnroll === true) {
+//     alert("Class is full sorry!");
+// } else {
+//     alert("Class has been added");
+// }
+
+// Product Offer
+
+//    var numberOfItems; // number
+//   var offerIsValid; // boolean
+//   var isPremiumMember; // boolean
+//   var productDiscountApplied = offerIsValid && (isPremiumMember || numberOfItems > 2);
+
+//Can the product offer be applied?
+
+//
+
+// var numberOfItems = prompt("How many items are you buying?");
+//
+// var offerIsValid = confirm("Is the offer valid?");
+//
+// var isPremiumMember = confirm("Are you a premium member");
+//
+// var productDiscountApplied = offerIsValid && (isPremiumMember || numberOfItems > 2);
+//
+// if (productDiscountApplied === true) {
+//     alert("Thank you for your purchase");
+// } else {
+//     alert("Sorry the offer is not valid");
+// }
+
+
+
+
