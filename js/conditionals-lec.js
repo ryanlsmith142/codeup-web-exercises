09904769b11eb3011e073dcac4dab2babe5ab55e
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-07 16:03:04 -0500

    Commit lecture notes to practice merge with master

---
+++ js/conditionals-lec.js
@@ -0,0 +1,145 @@
+"use strict";
+(function(){
+
+
+// var weShouldDeleteStuff = confirm('Are you sure you want to delete everything?');
+//
+// if (weShouldDeleteStuff) {
+//     // delete all the things...
+// } else {
+//     alert("Operation Canceled!");
+// }
+//
+// var isMarkSittingNearMe = confirm("Is Mark sitting near me?");
+//
+// if (isMarkSittingNearMe) {
+//     alert("This is going to be a terrible day!");
+// } else {
+//     alert("What a great day!");
+// }
+
+// var pizzaPreference = prompt("What kind of pizza do you like?");
+//
+// if (pizzaPreference === "pineapple and hot sauce") {
+//     alert("What a coincidence, that's my favorite!");
+// } else if (pizzaPreference === "cheese") {
+//     alert("Just plain cheese? Okay...");
+// } else {
+//     alert(pizzaPreference + " isn't my favorite, but let's order some!");
+// }
+
+//    Create a function that accepts a string that is a traffic light color (green, yellow, red) as an input. When this function is called, it should return a message informing the user what to do when approaching that color light at an intersection.
+// Use an if/else if/else statement.
+
+//    What do I do when I encounter a traffic light?
+
+//    What is my input?
+
+//    A string - a traffic light color "green" "yellow" "red"
+
+//    What is my output?
+
+//    a string - informed the user what to do when approaching that color light at an intersection
+
+    //
+    // function whatColorLight(lightColor) {
+    //     if (lightColor === "red") {
+    //         return "Stop!"
+    //     } else if(lightColor === "yellow") {
+    //         return "Slow down buckaroo!";
+    //     } else {
+    //         return "Go on through!";
+    //     }
+    //
+    // }
+    // var whatColor = whatColorLight(prompt("What color is the light?"));
+    //
+    // console.log(whatColor);
+
+   //  function whatColorLight(lightColor) {
+   //      switch (lightColor) {
+   //          case "red":
+   //              return "Stop!";
+   //              break;
+   //          case "yellow":
+   //              return "Slow down buckaroo!";
+   //              break;
+   //          case "green":
+   //              return "Go on through";
+   //              break;
+   //          default:
+   //              return "Hey that's not a traffic light color";
+   //              break;
+   //      }
+   //  }
+   //
+   //
+   // var whatColor = whatColorLight(prompt("What color is the light?"));
+   //
+   //  console.log(whatColor);
+
+    // var weather = "notsunny";
+    // var weathermessage = (weather === "sunny") ? "Have a nice day" : "Its raining";
+    //
+    // console.log(weathermessage);
+
+
+//     var message;
+//
+//     var success = true;
+//
+//     if (success) {
+//         message = "Operation was successful.";
+//     } else {
+//         message = "Oops, something went wrong.";
+//     }
+//     console.log(message);
+//
+// // the above if/else can be re-written as:
+//     var message = (success) ? "Operation was successful." : "Oops, something went wrong.";
+//
+
+    //
+    // function isShoppingDone(bool) {
+    //     return (bool) ? document.write("Child has earned $10 this week") : document.write("Child has earned $5 this week");
+    // }
+    //
+    //
+    // isShoppingDone(true);
+    //
+    // isShoppingDone(false);
+
+    // function weatherType(forecast) {
+    //     switch(forecast) {
+    //         case "Sunny":
+    //             return "Its nice and sunny outside today...";
+    //         case "Rainy":
+    //             return "Rain is falling outside...";
+    //         case "Snowing":
+    //             return "The snow is coming...";
+    //         case "Overcast":
+    //             return "It isn't raining...";
+    //         default:
+    //             return"Select the weather";
+    //
+    //     }
+    // }
+    //
+    // document.write(weatherType("Sunny"));
+
+    // function selectTheme(color) {
+    //
+    //     var temp;
+    //
+    //     if (color === "black" || "white" ) {
+    //         console.log(color);
+    //         temp = (color) ? "black" : "white";
+    //         console.log(temp);
+    //     } else {
+    //         alert("Enter a valid color");
+    //     }
+    //     return temp;
+    // }
+    //
+    // document.write(selectTheme("white"));
+})();
