3e309f40eb6cddf483bca6c3b7477700b1ca32a7
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-12 15:11:23 -0500

    Change file path in html and add for_loops.js

---
+++ js/for_loops.js
@@ -0,0 +1,7 @@
+function showMultiplicationTable(number) {
+    for(var x = 1; x <= 10; x++) {
+        console.log(number + " x " + x + " = " + (number * x));
+    }
+}
+
+showMultiplicationTable(7);
