0e51afb8ba242b44063a248685b778ba08b695f8
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-12 16:15:49 -0500

    finish number pyramid

--- js/for_loops.js
+++ js/for_loops.js
@@ -4,4 +4,47 @@ function showMultiplicationTable(number) {
     }
 }
 
-showMultiplicationTable(7);
+// showMultiplicationTable(7);
+
+// This is how you get a random number between 50 and 100
+// This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+
+// for(var i = 1; i <= 10;i++) {
+//     var randomNumber = Math.floor(Math.random() * 20) + 180;
+//     if(randomNumber % 2 === 0) {
+//         console.log(randomNumber + ' is even');
+//     } else {
+//         console.log(randomNumber + ' is odd');
+//     }
+// }
+
+// for(var i = 1; i < 10;i++) {
+//     var output = "";
+//     for (var j = 1; j <= i; j++) {
+//         output += j.toString() + i.toString() + "";
+//     }
+//     console.log(output);
+// }
+
+
+// for(var i = 1;i <= 10; i++) {
+//     var output = "";
+//     output += i.toString() + i.toString() + "";
+//     console.log(output);
+// }
+
+
+
+for(var x = 1; x <= 9; x++) {
+
+    output='';
+
+    for (y = 0; y < x; y++)
+    {
+        output = output + (x);
+    }
+
+    console.log(output);
+
+}
