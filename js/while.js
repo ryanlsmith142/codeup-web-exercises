5d131286e1793122446be411b9b2f4fa19b655fa
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-12 16:33:17 -0500

    changed code around to demonstrate

--- js/while.js
+++ js/while.js
@@ -15,17 +15,17 @@ var allCones = Math.floor(Math.random() * 50) + 50;
 
 
 
-// do {
-//     var customerCones = Math.floor(Math.random() * 5) + 1;
-//     if(customerCones > allCones) {
-//         console.log("Cannot sell you " + customerCones + " I only have " + allCones);
-//     } else {
-//         console.log(customerCones + " cone(s) sold!")
-//         allCones = allCones - customerCones;
-//     }
-//     console.log(allCones);
-// } while (allCones > 0) {
-//     console.log("Yay, I sold them all");
-// }
+do {
+    var customerCones = Math.floor(Math.random() * 5) + 1;
+    if(customerCones > allCones) {
+        console.log("Cannot sell you " + customerCones + " I only have " + allCones);
+    } else {
+        console.log(customerCones + " cone(s) sold!")
+        allCones = allCones - customerCones;
+    }
+    console.log(allCones);
+} while (allCones > 0) {
+    console.log("Yay, I sold them all");
+}
 
 
