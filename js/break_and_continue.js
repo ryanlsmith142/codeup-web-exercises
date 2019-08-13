1c055892553b4653b7bec67c5d54447ca4aa8028
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-13 10:19:24 -0500

    fix input that is able to be entered

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,19 +1,32 @@
 "use strict";
 
-var userNumber = prompt("give me an odd number!");
-
-for (var i = 1; 1 <= 50; i++) {
-    if (i % 2 === 0) {
-        continue;
-    }
-
-    if (parseFloat(userNumber) === i) {
-        console.log('Yikes! Skipping number: ' + i);
-    } else {
-        console.log("Here is an odd number: " + i);
-    }
-
-    if (i >= 49) {
-        break;
-    }
+function isString(value) {
+    return typeof value == "string";
 }
+
+var userNumber;
+var parsedInput;
+
+do {
+    userNumber = prompt("Give me an odd number!");
+    parsedInput = parseInt(userNumber);
+}
+while(userNumber % 2 === 0 || parsedInput < 0 || isNaN(parsedInput));
+
+    console.log("Number to skip is: " + userNumber);
+
+    for (var i = 1; 1 <= 50; i++) {
+        if (i % 2 === 0) {
+            continue;
+        }
+
+        if (parseFloat(userNumber) === i) {
+            console.log('Yikes! Skipping number: ' + i);
+        } else {
+            console.log("Here is an odd number: " + i);
+        }
+
+        if (i >= 49) {
+            break;
+        }
+    }
