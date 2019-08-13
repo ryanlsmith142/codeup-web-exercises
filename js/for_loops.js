08e548439f1eb3991b6dcf4798e844158b728215
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-13 11:08:29 -0500

    add different solutions

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,8 +10,8 @@ function showMultiplicationTable(number) {
 // This expression will generate a random number between 1 and 5
 // Math.floor(Math.random() * 5) + 1;
 
-// for(var i = 1; i <= 10;i++) {
-//     var randomNumber = Math.floor(Math.random() * 20) + 180;
+// for (var i = 1; i <= 10; i++) {
+//     var randomNumber = Math.floor(Math.random() * 181) + 20;
 //     if(randomNumber % 2 === 0) {
 //         console.log(randomNumber + ' is even');
 //     } else {
@@ -21,20 +21,34 @@ function showMultiplicationTable(number) {
 
 for(var x = 1; x <= 9; x++) {
 
-    output = '';
+    var output = '';
 
-    for (y = 0; y < x; y++)
+    for (var y = 0; y < x; y++)
     {
-        output = output + (x);
+        output = output + x;
     }
 
     console.log(output);
 
 }
 
+//OTHER SOLUTION
 
-    // for(var i = 100; i > 0;) {
-    //     console.log(i);
-    //     i = i - 5;
-    // }
+// var x = 1;
+//
+// for (var j = 1; j <= 9; j++) {
+//     console.log(j * x);
+//     x += "1";
+// }
+
+//OTHER SOLUTION
+
+// for (var i = 1; i <= 9; i++) {
+//     console.log(String(i).repeat(i));
+// }
+
+    for(var i = 100; i > 0;) {
+        console.log(i);
+        i = i - 5;
+    }
 
