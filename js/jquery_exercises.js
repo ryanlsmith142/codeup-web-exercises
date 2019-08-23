61659ac10ce1994729f0f637d6dc3edf1073a5d4
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-23 12:17:31 -0500

    finish exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
-    alert('Hey I\'m done');
 
+
+
+
+    // var element = $('#li-item1').html();
+    //
+    // alert(element);
+
+    $('.codeup').css('border', 'red 1px solid');
+
+
+    $('#title, #paragraph, #ul-list, #div1').css('border', '5px black solid');
 });
