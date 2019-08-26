3178f16aab08b2fe88b5cd25daa68cd70feb5b99
Ryan Smith <ryan.l.smith142@gmail.com>
2019-08-26 09:53:48 -0500

    finish events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -7,18 +7,35 @@ $(document).ready(function () {
     //
     // alert(element);
 
-    // $('.codeup').css('border', 'red px solid');
+    // $('.codeup').css('border', 'red 1px solid');
 
 
     // $('#title, #paragraph, #ul-list, #div1').css('border', '5px black solid');
 
     // $('li').css('font-size', '20px');
     //
-    $('h1, p, li').css('color', 'blue');
+    // $('h1, p, li').css('color', 'blue');
     //
     // var title = $('h1').html();
     //
     // alert(title);
+
+    $('h1').click(function() {
+        $(this).css('background', 'red');
+    });
+
+    $('p').dblclick(function() {
+        $(this).css('font-size', '18px');
+    });
+
+    $('li').hover(function() {
+        $(this).css('color', 'red');
+    }, //End of first function
+        function() {
+        $(this).css('color', 'black');
+        }
+    );
+
 
 
 });
