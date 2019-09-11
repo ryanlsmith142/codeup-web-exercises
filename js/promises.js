a85969cc2b2238a8d79de363a0e40e4fa94c750b
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-11 08:02:03 -0500

    add exercises

---
+++ js/promises.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
+
+
+
+
+
+
+function wait(number) {
+
+    return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+
+            resolve();
+
+        }, number); //setTimeout()
+
+}); //new promise()
+
+
+
+} //wait()
+
+
+
+
+
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+// fetch('https://swapi.co/api/people/1').then((info) => {
+//     return info.json();
+// }).then((data) => {
+//     console.log(data);
+// }).catch(() => {
+//     console.log('error');
+// });
+
+
+
+// 98c32379190d69592e02263c854c6a7fb5c8b67d GitHub API Key
+
+function lastCommit(gitHubUsername) {
+
+    return new Promise((resolve) => {
+
+        fetch(`https://api.github.com/users/${gitHubUsername}/events`, {headers: {'Authorization': 'token 98c32379190d69592e02263c854c6a7fb5c8b67d'}}).then((data) => {
+
+            return data.json();
+
+        }).then((userData) => {
+
+            userData.filter((user) => {
+                if(user.type === 'PushEvent') {
+                    return user.type;
+                }
+            }); //filter()
+        }) //fetch()
+    });//promise()
+} //lastCommit
+
+console.log(lastCommit('ryanlsmith142'));
